497f967c8777198efcbc605d9c006bf23c511ec9
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2019-05-29 10:12:32 +0200

    Update server.R

--- server.R
+++ server.R
@@ -61,6 +61,13 @@ server <- function(input, output) {
         common_col = "ensembl_gene_id"
         savedcolname = colnames(data1)[input$colnb]
         colnames(data1) = c("ensembl_gene_id",colnames(data1)[-1])
+        if(grep(".",data1[1,input$colnb])==1){
+            data1[,input$colnb] = lapply(strsplit(as.character(data1[,input$colnb]), split = ".", fixed = TRUE), "[", 1)
+        } else{
+            return("Problem with ID not in the good format should be like: ENSMUSG00000000001.")
+        }
+          
+          
       } else {
         common_col = "external_gene_name"
         savedcolname = colnames(data1)[input$colnb]
